b634b95ba8c6d47cbace392f978cf9a64160a6c7
antonio-amjr <116589331+antonio-amjr@users.noreply.github.com>
2025-10-30 09:59:41 -0300

    Updating TCDD_12 Import For Backend's Change (#723)
    * Updating TCDD_12 test case import to handle a future change in the backend
    
    * Updating Copyright
    
    Updating date year in the Copyright message.

--- onboarding_payload_test_suite/tcdd12/tcdd12.py
+++ onboarding_payload_test_suite/tcdd12/tcdd12.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2023 Project CHIP Authors
+# Copyright (c) 2025 Project CHIP Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from app.models.test_enums import TestStateEnum
+from app.models import TestStateEnum
 from app.test_engine.logger import test_engine_logger as logger
 from app.test_engine.models import TestStep
 from app.user_prompt_support import PromptRequest, TextInputPromptRequest
@@ -196,3 +196,4 @@ class TCDD12(PayloadParsingTestBaseClass):
             timeout=PROMPT_TIMEOUT,
         )
         return prompt_request
+
